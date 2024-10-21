2e00c94b7f4563ea616d6cddaab501d77082682b
Kausar Khan <156500913+KausarKhan-2002@users.noreply.github.com>
2024-10-21 12:14:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	 let obj = {};
+  for (let chr of str) {
+    if (obj[chr]) obj[chr] += 1;
+    else obj[chr] = 1;
+  }
+
+  for (let val in obj) {
+    // console.log(obj[val]);
+
+    if (obj[val] == 1) return val;
+  }
+  return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
